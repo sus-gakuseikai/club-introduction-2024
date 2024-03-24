d6b73b95452b5ad98ed3b219401e5e13fee00b29
shima3141 <114070006+shima3141@users.noreply.github.com>
2024-03-24 21:32:35 +0900

    ホームページ作成

---
+++ js/publish_data.js
@@ -0,0 +1,60 @@
+const publish_info = [
+  {
+    category: '総務系',
+    clubs: [
+      
+    ]
+  },
+  {
+    category: '運動系',
+    clubs: [
+      { clubName: 'クライミング部',
+        id: 'climb',
+        isMovie: false,
+        movieUrl: '',
+        links: [
+          {
+            label: 'Twitter',
+            url: 'https://x.com/suwarikaclimb'
+          },
+        ]
+      },
+      { clubName: 'ソフトテニス部',
+        id: 'tennis',
+        isMovie: false,
+        movieUrl: '',
+        links: [
+          {
+            label: 'Twitter',
+            url: 'https://twitter.com/suwarikasoftten'
+          },
+        ]
+      },
+    ]
+  },
+  {
+    category: "文化系",
+    clubs: [
+      { clubName: 'ロボット研究部',
+        id: 'robot',
+        isMovie: false,
+        movieUrl: '',
+        links: [
+        ]
+      },
+      { clubName: '写真部',
+        id: 'photo',
+        isMovie: false,
+        movieUrl: '',
+        links: [
+        ]
+      },
+    ]
+  },
+  {
+    category: "外部団体",
+    clubs: [
+
+    ]
+  },
+]
